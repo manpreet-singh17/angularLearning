1168275bd93e26faadb0f7c7b91a107a7161fd4b
manpreet singh <mpsingh0001@gmail.com>
2017-04-29 10:32:18 +1200

    upto watch method all done.

--- module2Lab/app/menuController.js
+++ module2Lab/app/menuController.js
@@ -1,6 +1,19 @@
 app.controller('menuController', [
     '$scope',
     function($scope) {
-        $scope.model = { title: 'Our Menu' }
+        $scope.model = { title: 'Our Menu' };
+        $scope.changeMainDish = function(item) {
+            $scope.model.mainDish = item;
+        };
+
+        $scope.$watch("model.mainDish", function(newValue, oldValue) {
+            if (newValue === "BBQ Pizza") {
+                $scope.Note = "| This pizza is non veg.";
+                //alert("You have selected the non veg BBQ pizza!");
+            } else {
+                $scope.Note = "";
+            }
+
+        });
     }
 ]);
