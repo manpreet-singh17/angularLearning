a4aef06bf789fde20303d919f05196f266d9da3d
manpreet singh <mpsingh0001@gmail.com>
2017-04-29 11:07:40 +1200

    module 2 two-way binding is done.

--- module2Lab/app/menuController.js
+++ module2Lab/app/menuController.js
@@ -2,6 +2,7 @@ app.controller('menuController', [
     '$scope',
     function($scope) {
         $scope.model = { title: 'Our Menu' };
+
         $scope.changeMainDish = function(item) {
             $scope.model.mainDish = item;
         };
